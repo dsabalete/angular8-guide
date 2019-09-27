77ca3a747a23a53d2b7c5b17ca481db7d831f072
David Sabalete <dsabalete@gmail.com>
2019-09-28 00:32:51 +0200

    Ad list of recipes with ngFor

--- project-course/src/app/recipes/recipe-list/recipe-list.component.ts
+++ project-course/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe-model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe','This is a simple test','https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'),
+    new Recipe('Another Recipe','This is another simple test','https://c.pxhere.com/images/15/3d/9ee477ee62341b9480ce314b02f8-1417897.jpg!d')
+  ];
 
   constructor() { }
 
