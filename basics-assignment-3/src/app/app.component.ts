f95028302592abc19f0886247ab8ce9c7e40606b
David Sabalete <dsabalete@gmail.com>
2019-09-27 21:24:21 +0200

    my solution for project 3

--- basics-assignment-3/src/app/app.component.ts
+++ basics-assignment-3/src/app/app.component.ts
@@ -6,4 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  showPassword = false;
+  loglines = [];
+  counter = 0;
+
+  toggle() {
+    this.showPassword = !this.showPassword;
+    this.counter++;
+    this.loglines.push({ time: new Date(), counter: this.counter });
+  }
 }
